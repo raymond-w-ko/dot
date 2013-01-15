a221232bc4197056ebf1f0f981aa22671c723a48
Raymond W. Ko <raymond.w.ko@gmail.com>
2013-01-15 17:34:31 -0500

    safer upstream setting in case submodule not initalized

--- vim/bundle/set_upstreams.sh
+++ vim/bundle/set_upstreams.sh
@@ -6,20 +6,26 @@ cd "$DIR"
 if [ -d "./Lucius" ]; then
     cd Lucius
     pwd
-    git remote add upstream git://github.com/vim-scripts/Lucius.git
+    if [ -e ".git" ]; then
+        git remote add upstream git://github.com/vim-scripts/Lucius.git
+    fi
     cd ..
 fi
 
 if [ -d "./bufkill.vim" ]; then
     cd bufkill.vim
     pwd
-    git remote add upstream git://github.com/vim-scripts/bufkill.vim.git
+    if [ -e ".git" ]; then
+        git remote add upstream git://github.com/vim-scripts/bufkill.vim.git
+    fi
     cd ..
 fi
 
 if [ -d "./vim-foreplay" ]; then
     cd vim-foreplay
     pwd
-    git remote add upstream git://github.com/tpope/vim-foreplay.git
+    if [ -e ".git" ]; then
+        git remote add upstream git://github.com/tpope/vim-foreplay.git
+    fi
     cd ..
 fi
