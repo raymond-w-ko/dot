e5187bca5472b995ecde82c2972047fbcd76bf29
Raymond W. Ko <raymond.w.ko@gmail.com>
2013-12-14 16:31:51 -0500

    start creating machine specific forks of resource config files

--- bin/dot_files_bootstrap.sh
+++ bin/dot_files_bootstrap.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 set -e
 
+if [[ -z "$HOME" ]]; then
+  echo '$HOME not set'
+  exit 1
+fi
+
+if [[ -z "$1" ]]; then
+  echo "dot_files_bootstrap.sh machine_id"
+  exit 1
+fi
+
 function ensure_link {
-    test -L "$HOME/$2" || ln -s "$HOME/$1" "$HOME/$2"
+  test -L "$HOME/$2" || ln -s "$HOME/$1" "$HOME/$2"
 }
 
 cd "$HOME"
 mkdir -p ~/src
 
 if [[ `uname` == 'Darwin' ]]; then
-mkdir -p ~/Library/Vim/backup
-    mkdir -p ~/Library/Vim/swap
-    mkdir -p ~/Library/Vim/undo
+  mkdir -p ~/Library/Vim/backup
+  mkdir -p ~/Library/Vim/swap
+  mkdir -p ~/Library/Vim/undo
 else
-mkdir -p ~/.local/share/vim/backup
-    mkdir -p ~/.local/share/vim/swap
-    mkdir -p ~/.local/share/vim/undo
+  mkdir -p ~/.local/share/vim/backup
+  mkdir -p ~/.local/share/vim/swap
+  mkdir -p ~/.local/share/vim/undo
 fi
 
+# VIM
 ensure_link "vim/bin/ex" "bin/ex"
 ensure_link "vim/bin/rview" "bin/rview"
 ensure_link "vim/bin/rvim" "bin/rvim"
@@ -25,3 +36,6 @@ ensure_link "vim/bin/vim" "bin/vim"
 ensure_link "vim/bin/vimdiff" "bin/vimdiff"
 ensure_link "vim/bin/vimtutor" "bin/vimtutor"
 ensure_link "vim/bin/xxd" "bin/xxd"
+
+# machine specific symbolic links
+ensure_link ".xinitrc.$1" ".xinitrc.extra"
