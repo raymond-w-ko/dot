0f38cff59356e6a5d67504f6b0a9bb23956478f2
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-01-20 19:54:12 -0500

    make set_upstream.sh more concise by implementing a bash function

--- .vim/bundle/set_upstreams.sh
+++ .vim/bundle/set_upstreams.sh
@@ -2,23 +2,25 @@
 
 set -e
 
+function set_upstream_git() {
+  if [ -d "$1" ]; then
+      cd "$1"
+      pwd
+      if [ -e ".git" ]; then
+          set +e
+          git remote rm upstream
+          git remote add upstream "$2"
+          set -e
+          git remote -v
+      fi
+      cd ..
+  fi
+  echo "--------------------------------------------------------------------------------"
+}
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
-if [ -d "./Lucius" ]; then
-    cd Lucius
-    pwd
-    if [ -e ".git" ]; then
-        git remote add upstream git://github.com/vim-scripts/Lucius.git
-    fi
-    cd ..
-fi
-
-if [ -d "./bufkill.vim" ]; then
-    cd bufkill.vim
-    pwd
-    if [ -e ".git" ]; then
-        git remote add upstream git://github.com/vim-scripts/bufkill.vim.git
-    fi
-    cd ..
-fi
+set_upstream_git './Lucius' 'git://github.com/vim-scripts/Lucius.git'
+set_upstream_git './seoul256.vim' 'git://github.com/junegunn/seoul256.vim.git'
+set_upstream_git './bufkill.vim' 'git://github.com/vim-scripts/bufkill.vim.git'
