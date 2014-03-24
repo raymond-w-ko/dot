3dc3bec8ac7304fa96e5f60cfd144d95c6102e23
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-03-24 12:56:56 -0400

    script for resetting keyboard settings

---
+++ bin/keyboard_settings.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# does not work for hot plugged keyboards (like for my netbook)
+#xmodmap ~/.xmodmaprc
+# nice, but conflicts with general Windows usage
+#setxkbmap -option -option ctrl:nocaps,altwin:swap_alt_win
+setxkbmap -option -option ctrl:nocaps
+
+xset r rate 333 32
