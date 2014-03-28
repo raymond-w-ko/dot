a18c7bf4f59a27eba8f6cc63d23c45e9a3033e30
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-03-27 20:53:42 -0400

    turn on ncache, works for rog

--- bin/startvnc0.sh
+++ bin/startvnc0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #x0vncserver -display :0 -passwordfile ~/.vnc/passwd
-x11vnc -display :0 -rfbauth $HOME/.vnc/passwd -repeat
+x11vnc -display :0 -rfbauth $HOME/.vnc/passwd -repeat -ncache 10 -ncache_cr
