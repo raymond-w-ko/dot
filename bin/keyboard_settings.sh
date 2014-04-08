911260e6aae85967e507efb0357fb16646721eff
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-04-08 09:48:06 -0400

    double

--- bin/keyboard_settings.sh
+++ bin/keyboard_settings.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-# does not work for hot plugged keyboards (like for my netbook)
-#xmodmap ~/.xmodmaprc
-# nice, but conflicts with general Windows usage
-#setxkbmap -option -option ctrl:nocaps,altwin:swap_alt_win
-setxkbmap -option -option ctrl:nocaps
+function config_keyboard()
+{
+  # does not work for hot plugged keyboards (like for my netbook)
+  #xmodmap ~/.xmodmaprc
+  # nice, but conflicts with general Windows usage
+  #setxkbmap -option -option ctrl:nocaps,altwin:swap_alt_win
+  setxkbmap -option -option ctrl:nocaps
 
-xset r rate 333 32
+  xset r rate 333 32
 
-killall xbindkeys
-xbindkeys
+  killall xbindkeys
+  xbindkeys
+}
+
+config_keyboard
+sleep 1
+config_keyboard
