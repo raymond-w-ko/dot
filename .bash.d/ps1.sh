de6f68819d0f4b770601b6fbbba78a5478b2951a
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-04-24 20:18:40 -0400

    hopefully fix up history corruption of prompt in bash, refactor color names

--- .bash.d/ps1.sh
+++ .bash.d/ps1.sh
@@ -9,27 +9,27 @@ HASH=`echo $HOSTNAME | ${MD5}`
 RGB=${HASH:0:6}
 COLORED_HOST=$(~/bin/rgb2term.py $RGB)$HOSTNAME
 
+_WhiteBold="\[\033[1;37m\]"
+_White="\[\033[0;37m\]"
+_BlueBold="\[\033[1;34m\]"
+_Blue="\[\033[0;34m\]"
+_GreenBold="\[\033[1;32m\]"
+_Green="\[\033[0;32m\]"
+_RedBold="\[\033[1;31m\]"
+_YellowBold="\[\033[1;33m\]"
+_Yellow="\[\033[0;33m\]"
+_CyanBold="\[\033[1;36m\]"
+_Cyan="\[\033[0;36m\]"
+_Purple="\[\033[0;35m\]"
+_PurpleBold="\[\033[1;35m\]"
+_NormalColor="\[\033[0m\]"
+#_Dash="\342\224\200"
+_Dash="-"
+
 function prompt_command {
   ACTUAL_LAST_RET=$?
 
-  whiteBold="\[\033[1;37m\]"
-  white="\[\033[0;37m\]"
-  blueBold="\[\033[1;34m\]"
-  blue="\[\033[0;34m\]"
-  greenBold="\[\033[1;32m\]"
-  green="\[\033[0;32m\]"
-  redBold="\[\033[1;31m\]"
-  yellowBold="\[\033[1;33m\]"
-  yellow="\[\033[0;33m\]"
-  cyanBold="\[\033[1;36m\]"
-  cyan="\[\033[0;36m\]"
-  purple="\[\033[0;35m\]"
-  purpleBold="\[\033[1;35m\]"
-  normalColor="\[\033[0m\]"
-  #dash="\342\224\200"
-  dash="-"
-
-  RET_STATUS="\$(if [[ $ACTUAL_LAST_RET == 0 ]]; then echo -n \"$greenBold\"; echo ret: $ACTUAL_LAST_RET; else echo -n \"$redBold\"; echo ret: $ACTUAL_LAST_RET; fi)"
+  RET_STATUS="\$(if [[ $ACTUAL_LAST_RET == 0 ]]; then echo -n \"$_GreenBold\"; echo ret: $ACTUAL_LAST_RET; else echo -n \"$_RedBold\"; echo ret: $ACTUAL_LAST_RET; fi)"
   function BatteryStatus {
     if hash acpi 2>/dev/null; then
       acpi | sed 's/Battery 0: //' | sed 's/ remaining//'
@@ -61,15 +61,15 @@ function prompt_command {
       echo ""
     fi
   }
-  ROOT_WARNING="$(if [[ ${EUID} == 0 ]]; then echo "$redBold*** ROOT *** "; else echo ''; fi)"
-  USER_AT_HOST="$(if [[ ${EUID} == 0 ]]; then echo "$redBold\u"; else echo "$blue\u"; fi)$blue @ $COLORED_HOST"
+  ROOT_WARNING="$(if [[ ${EUID} == 0 ]]; then echo "$_RedBold*** ROOT *** "; else echo ''; fi)"
+  USER_AT_HOST="$(if [[ ${EUID} == 0 ]]; then echo "$_RedBold\u"; else echo "$_Blue\u"; fi)$_Blue @ $COLORED_HOST"
   BATTERY="\$(BatteryStatus)"
   FILE_INFO="\$(ls -1 2>/dev/null | wc -l | sed 's: ::g') files, \$(ls -lah 2>/dev/null | grep -m 1 total | sed 's/total //')b"
-  LINE1="$ROOT_WARNING$whiteBold($cyan\D{%Y %b %e %l:%M:%S %p}$whiteBold)$dash($green$BATTERY$whiteBold)"
-  #LINE2="($yellow$FILE_INFO$whiteBold)$dash$green\$(GitBranch)$whiteBold$dash($yellow\w$white$whiteBold)"
-  LINE2="$ROOT_WARNING$green\$(GitBranch)$whiteBold$dash$green\$(HgBranch)$whiteBold$dash($yellow\w$white$whiteBold)"
-  LINE3="$ROOT_WARNING$whiteBold$USER_AT_HOST$whiteBold ($white$RET_STATUS$whiteBold)"
-  PROMPT=" $dash> $normalColor"
+  LINE1="$ROOT_WARNING$_WhiteBold($_Cyan\D{%Y %b %e %l:%M:%S %p}$_WhiteBold)$_Dash($_Green$BATTERY$_WhiteBold)"
+  #LINE2="($_Yellow$FILE_INFO$_WhiteBold)$_Dash$_Green\$(GitBranch)$_WhiteBold$_Dash($_Yellow\w$_White$_WhiteBold)"
+  LINE2="$ROOT_WARNING$_Green\$(GitBranch)$_WhiteBold$_Dash$_Green\$(HgBranch)$_WhiteBold$_Dash($_Yellow\w$_White$_WhiteBold)"
+  LINE3="$ROOT_WARNING$_WhiteBold$USER_AT_HOST$_WhiteBold ($_White$RET_STATUS$_WhiteBold)"
+  PROMPT=" $_Dash> $_NormalColor"
   export PS1="\n$LINE1\n$LINE2\n$LINE3$PROMPT`echo $REAL_LAST_RET`"
 }
 export PROMPT_COMMAND=prompt_command
--- bin/rgb2term.py
+++ bin/rgb2term.py
@@ -4,5 +4,5 @@ import sys
 
 if __name__ == '__main__':
     short, rgb = colorhelper.rgb2short(sys.argv[1])
-    code = '\\033[38;5;%sm' % (short)
+    code = '\\[\\033[38;5;%sm\\]' % (short)
     sys.stdout.write(code)
