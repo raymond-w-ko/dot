77236c42761a2e1191bb32a988d30b9d198ff2fc
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-06-26 09:04:25 -0400

    maybe fix crashing errors?

--- bin/my_keyboard_settings_daemon.py
+++ bin/my_keyboard_settings_daemon.py
@@ -12,29 +12,34 @@ def kbd_event(action, device):
 
     if action == 'add':
         ticks_before_reapplying_settings = 2
+        print(device)
 
 def apply_keyboard_settings():
     global ticks_before_reapplying_settings
 
     while True:
-        time.sleep(0.25)
+        try:
+            time.sleep(1)
 
-        if ticks_before_reapplying_settings < 0:
-            continue
-        elif ticks_before_reapplying_settings > 0:
-            ticks_before_reapplying_settings = ticks_before_reapplying_settings - 1
-            continue
+            if ticks_before_reapplying_settings < 0:
+                continue
+            elif ticks_before_reapplying_settings > 0:
+                ticks_before_reapplying_settings = ticks_before_reapplying_settings - 1
+                continue
 
-        ticks_before_reapplying_settings = -1
+            ticks_before_reapplying_settings = -1
 
-        subprocess.call('setxkbmap -option -option ctrl:nocaps -option altwin:swap_alt_win', shell = True)
-        subprocess.call('xset r rate 333 32', shell = True)
-        subprocess.call('killall xcape', shell = True)
-        subprocess.call("xcape -t 333 -e 'Control_L=Escape;Shift_L=Shift_L|minus'", shell = True)
-        subprocess.call('killall xbindkeys', shell = True)
-        subprocess.call('xbindkeys', shell = True)
+            subprocess.call('setxkbmap -option -option ctrl:nocaps -option altwin:swap_alt_win', shell = True)
+            subprocess.call('xset r rate 333 32', shell = True)
+            subprocess.call('killall xcape', shell = True)
+            subprocess.call("xcape -t 333 -e 'Control_L=Escape;Shift_L=Shift_L|minus'", shell = True)
+            subprocess.call('killall xbindkeys', shell = True)
+            subprocess.call('xbindkeys', shell = True)
 
-        print('applied keyboard settings')
+            print('applied keyboard settings')
+        except:
+            print('swallowed exception in apply_keyboard_settings thread')
+            pass
 
 def main():
     t = threading.Thread(target=apply_keyboard_settings)
@@ -51,6 +56,7 @@ def main():
             for action, device in monitor:
                 pass
         except:
+            print('swallowed exception in main thread')
             pass
 
 if __name__ == '__main__':
