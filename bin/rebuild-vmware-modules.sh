37f04708ab075c3d8fbded79ef026166fb870309
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-07-03 10:33:47 -0400

    reload systemd stuff first

--- bin/rebuild-vmware-modules.sh
+++ bin/rebuild-vmware-modules.sh
@@ -3,5 +3,6 @@
 set -e
 sudo vmware-modconfig --console --install-all
 set +e
+sudo systemctl daemon-reload
 sudo systemctl restart vmware-usbarbitrator.service
 sudo systemctl restart vmware.service
