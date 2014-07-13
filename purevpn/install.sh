c8a496f6965e9ec21bcf0dc1fe699b162eb5e968
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-07-12 23:19:46 -0400

    add install script

---
+++ purevpn/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+ORIGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd /etc/openvpn/
+
+sudo rm -f ca.crt
+sudo ln -s "$ORIGDIR/ca.crt"
+
+sudo rm -f Wdc.key
+sudo ln -s "$ORIGDIR/Wdc.key"
+
+sudo rm -f passwd
+sudo ln -s "$HOME/.ssh/purevpn" passwd
+
+sudo rm -f client.conf
+sudo ln -s "$ORIGDIR/UDP/USA-New York-UDP.ovpn" client.conf
