6e49c1dd7be904b4b01de49519e14f2b170f8b29
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-07-14 10:12:20 -0400

    re-indenting

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -15,24 +15,24 @@ local menubar = require("menubar")
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
+  naughty.notify({ preset = naughty.config.presets.critical,
+                 title = "Oops, there were errors during startup!",
+                 text = awesome.startup_errors })
 end
 
 -- Handle runtime errors after startup
 do
-    local in_error = false
-    awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
+  local in_error = false
+  awesome.connect_signal("debug::error", function (err)
+    -- Make sure we don't go into an endless error loop
+    if in_error then return end
+    in_error = true
 
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = err })
-        in_error = false
-    end)
+    naughty.notify({ preset = naughty.config.presets.critical,
+                   title = "Oops, an error happened!",
+                   text = err })
+    in_error = false
+  end)
 end
 -- }}}
 
@@ -57,26 +57,26 @@ local wheel_up_button = 4
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts =
 {
-    awful.layout.suit.max,
-    awful.layout.suit.tile,
-    --awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.floating,
-    --awful.layout.suit.tile.top,
-    --awful.layout.suit.fair,
-    --awful.layout.suit.fair.horizontal,
-    --awful.layout.suit.spiral,
-    --awful.layout.suit.spiral.dwindle,
-    --awful.layout.suit.max.fullscreen,
-    --awful.layout.suit.magnifier,
+  awful.layout.suit.max,
+  awful.layout.suit.tile,
+  --awful.layout.suit.tile.left,
+  awful.layout.suit.tile.bottom,
+  awful.layout.suit.floating,
+  --awful.layout.suit.tile.top,
+  --awful.layout.suit.fair,
+  --awful.layout.suit.fair.horizontal,
+  --awful.layout.suit.spiral,
+  --awful.layout.suit.spiral.dwindle,
+  --awful.layout.suit.max.fullscreen,
+  --awful.layout.suit.magnifier,
 }
 -- }}}
 
 -- {{{ Wallpaper
 if beautiful.wallpaper then
-    for s = 1, screen.count() do
-        gears.wallpaper.maximized(beautiful.wallpaper, s, true)
-    end
+  for s = 1, screen.count() do
+    gears.wallpaper.maximized(beautiful.wallpaper, s, true)
+  end
 end
 -- }}}
 
@@ -84,24 +84,27 @@ end
 -- Define a tag table which hold all screen tags.
 tags = {}
 for s = 1, screen.count() do
-    -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+  -- Each screen has its own tag table.
+  tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
 end
 -- }}}
 
 -- {{{ Menu
 -- Create a laucher widget and a main menu
 myawesomemenu = {
-   --{ "manual", terminal .. " -e man awesome" },
-   --{ "edit config", editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", awesome.quit }
+  --{ "manual", terminal .. " -e man awesome" },
+  --{ "edit config", editor_cmd .. " " .. awesome.conffile },
+  { "restart", awesome.restart },
+  { "quit", awesome.quit }
 }
 
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "open terminal", terminal }
-                                  }
-                        })
+mymainmenu = awful.menu(
+  {
+    items = {
+      { "awesome", myawesomemenu, beautiful.awesome_icon },
+      { "open terminal", terminal }
+    }
+  })
 
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
                                      menu = mymainmenu })
@@ -118,117 +121,117 @@ mypromptbox = {}
 mylayoutbox = {}
 mytaglist = {}
 mytaglist.buttons = awful.util.table.join(
-                    awful.button({ }, 1, awful.tag.viewonly),
-                    awful.button({ modkey }, 1, awful.client.movetotag),
-                    awful.button({ }, 3, awful.tag.viewtoggle),
-                    awful.button({ modkey }, 3, awful.client.toggletag),
-                    awful.button({ }, wheel_down_button, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
-                    awful.button({ }, wheel_up_button, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end)
-                    )
+  awful.button({ }, 1, awful.tag.viewonly),
+  awful.button({ modkey }, 1, awful.client.movetotag),
+  awful.button({ }, 3, awful.tag.viewtoggle),
+  awful.button({ modkey }, 3, awful.client.toggletag),
+  awful.button({ }, wheel_down_button, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
+  awful.button({ }, wheel_up_button, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end)
+  )
 mytasklist = {}
 mytasklist.buttons = awful.util.table.join(
-                     awful.button({ }, 1, function (c)
-                                              if c == client.focus then
-                                                  c.minimized = true
-                                              else
-                                                  -- Without this, the following
-                                                  -- :isvisible() makes no sense
-                                                  c.minimized = false
-                                                  if not c:isvisible() then
-                                                      awful.tag.viewonly(c:tags()[1])
-                                                  end
-                                                  -- This will also un-minimize
-                                                  -- the client, if needed
-                                                  client.focus = c
-                                                  c:raise()
-                                              end
-                                          end),
-                     awful.button({ }, 3, function ()
-                                              if instance then
-                                                  instance:hide()
-                                                  instance = nil
-                                              else
-                                                  instance = awful.menu.clients({
-                                                      theme = { width = 250 }
-                                                  })
-                                              end
-                                          end),
-                     awful.button({ }, wheel_down_button, function ()
-                                              awful.client.focus.byidx(1)
-                                              if client.focus then client.focus:raise() end
-                                          end),
-                     awful.button({ }, wheel_up_button, function ()
-                                              awful.client.focus.byidx(-1)
-                                              if client.focus then client.focus:raise() end
-                                          end))
+  awful.button({ }, 1, function (c)
+    if c == client.focus then
+      c.minimized = true
+    else
+      -- Without this, the following
+      -- :isvisible() makes no sense
+      c.minimized = false
+      if not c:isvisible() then
+        awful.tag.viewonly(c:tags()[1])
+      end
+      -- This will also un-minimize
+      -- the client, if needed
+      client.focus = c
+      c:raise()
+    end
+  end),
+  awful.button({ }, 3, function ()
+    if instance then
+      instance:hide()
+      instance = nil
+    else
+      instance = awful.menu.clients({
+                                    theme = { width = 250 }
+                                  })
+    end
+  end),
+  awful.button({ }, wheel_down_button, function ()
+    awful.client.focus.byidx(1)
+    if client.focus then client.focus:raise() end
+  end),
+  awful.button({ }, wheel_up_button, function ()
+    awful.client.focus.byidx(-1)
+    if client.focus then client.focus:raise() end
+  end))
 
 local info_widgets = require('mywidgets')
 
 for s = 1, screen.count() do
-    -- Create a promptbox for each screen
-    mypromptbox[s] = awful.widget.prompt()
-    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    mylayoutbox[s] = awful.widget.layoutbox(s)
-    mylayoutbox[s]:buttons(awful.util.table.join(
-                           awful.button({ }, 1, function () awful.layout.inc(layouts, 1) end),
-                           awful.button({ }, 3, function () awful.layout.inc(layouts, -1) end),
-                           awful.button({ }, wheel_down_button, function () awful.layout.inc(layouts, -1) end),
-                           awful.button({ }, wheel_up_button, function () awful.layout.inc(layouts, 1) end)))
-    -- Create a taglist widget
-    mytaglist[s] = awful.widget.taglist(s, awful.widget.taglist.filter.all, mytaglist.buttons)
+  -- Create a promptbox for each screen
+  mypromptbox[s] = awful.widget.prompt()
+  -- Create an imagebox widget which will contains an icon indicating which layout we're using.
+  -- We need one layoutbox per screen.
+  mylayoutbox[s] = awful.widget.layoutbox(s)
+  mylayoutbox[s]:buttons(awful.util.table.join(
+    awful.button({ }, 1, function () awful.layout.inc(layouts, 1) end),
+    awful.button({ }, 3, function () awful.layout.inc(layouts, -1) end),
+    awful.button({ }, wheel_down_button, function () awful.layout.inc(layouts, -1) end),
+    awful.button({ }, wheel_up_button, function () awful.layout.inc(layouts, 1) end)))
+  -- Create a taglist widget
+  mytaglist[s] = awful.widget.taglist(s, awful.widget.taglist.filter.all, mytaglist.buttons)
 
-    -- Create the wibox
-    bottom_wibox[s] = awful.wibox({ position = "bottom", screen = s })
+  -- Create the wibox
+  bottom_wibox[s] = awful.wibox({ position = "bottom", screen = s })
 
-    -- Widgets that are aligned to the left
-    local left_layout = wibox.layout.fixed.horizontal()
-    left_layout:add(mylauncher)
-    left_layout:add(mylayoutbox[s])
-    left_layout:add(mytaglist[s])
-    left_layout:add(mypromptbox[s])
+  -- Widgets that are aligned to the left
+  local left_layout = wibox.layout.fixed.horizontal()
+  left_layout:add(mylauncher)
+  left_layout:add(mylayoutbox[s])
+  left_layout:add(mytaglist[s])
+  left_layout:add(mypromptbox[s])
 
-    -- Widgets that are aligned to the right
-    local right_layout = wibox.layout.fixed.horizontal()
+  -- Widgets that are aligned to the right
+  local right_layout = wibox.layout.fixed.horizontal()
 
-    for _, widget in ipairs(info_widgets) do
-        right_layout:add(widget)
-    end
+  for _, widget in ipairs(info_widgets) do
+    right_layout:add(widget)
+  end
 
-    if s == 1 then
-        local separator = wibox.widget.textbox()
-        separator:set_text(' :: ')
-        right_layout:add(separator)
+  if s == 1 then
+    local separator = wibox.widget.textbox()
+    separator:set_text(' :: ')
+    right_layout:add(separator)
 
-        right_layout:add(wibox.widget.systray())
-    end
+    right_layout:add(wibox.widget.systray())
+  end
 
-    -- Now bring it all together (with the tasklist in the middle)
-    local layout = wibox.layout.align.horizontal()
-    layout:set_left(left_layout)
-    --layout:set_middle(mytasklist[s])
-    layout:set_right(right_layout)
+  -- Now bring it all together (with the tasklist in the middle)
+  local layout = wibox.layout.align.horizontal()
+  layout:set_left(left_layout)
+  --layout:set_middle(mytasklist[s])
+  layout:set_right(right_layout)
 
-    bottom_wibox[s]:set_widget(layout)
+  bottom_wibox[s]:set_widget(layout)
 
-    -- create top toolbar with just tasklist
+  -- create top toolbar with just tasklist
 
-    -- Create a tasklist widget
-    mytasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, mytasklist.buttons)
+  -- Create a tasklist widget
+  mytasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, mytasklist.buttons)
 
-    top_wibox[s] = awful.wibox({ position = "top", screen = s })
-    local layout = wibox.layout.fixed.horizontal()
-    layout:add(mytasklist[s])
+  top_wibox[s] = awful.wibox({ position = "top", screen = s })
+  local layout = wibox.layout.fixed.horizontal()
+  layout:add(mytasklist[s])
 
-    top_wibox[s]:set_widget(layout)
+  top_wibox[s]:set_widget(layout)
 end
 -- }}}
 
 -- {{{ Mouse bindings
 root.buttons(awful.util.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, wheel_down_button, awful.tag.viewnext),
-    awful.button({ }, wheel_up_button, awful.tag.viewprev)
+  awful.button({ }, 3, function () mymainmenu:toggle() end),
+  awful.button({ }, wheel_down_button, awful.tag.viewnext),
+  awful.button({ }, wheel_up_button, awful.tag.viewprev)
 ))
 -- }}}
 
