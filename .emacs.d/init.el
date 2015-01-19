8ba80d0bfb8bd547cf9b4184422d56980029f82a
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-01-19 13:15:43 -0500

    broken for now

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,12 +9,13 @@
 (require 'cl)
 (defvar packages-list
   '(evil
-     evil-leader
-     evil-tabs
-     evil-paredit
-     evil-surround
-     smooth-scrolling
-     paredit)
+    evil-leader
+    evil-tabs
+    evil-paredit
+    evil-surround
+    smooth-scrolling
+    paredit
+    color-theme-solarized)
   "List of packages needs to be installed at launch")
 
 (defun has-package-not-installed ()
@@ -37,11 +38,21 @@
 (setq uniquify-buffer-name-style 'post-forward
       uniquify-separator ":")
 
-(when (eq system-type 'gnu/linux)
-  (custom-set-faces
-   '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width semi-condensed :foundry "Misc" :family "Fixed"))))))
-
 (menu-bar-mode -1)
 (tool-bar-mode -1)
+(scroll-bar-mode -1)
 (setq inhibit-startup-screen t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(frame-background-mode (quote dark)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width semi-condensed :foundry "Misc" :family "Fixed")))))
 
+(load-theme 'solarized t)
