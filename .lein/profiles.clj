c2c9c0354f2826395280498fae263cdd939e27b4
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-03-23 10:28:44 -0400

    cljfmt probably isn't strictly necessary

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
                   [lein-cljfmt "0.1.10"]
                   [venantius/ultra "0.3.3"]]
-        :dependencies [[cljfmt "0.1.10"]]
+        :dependencies []
         :ultra {:color-scheme :solarized_dark}}}
