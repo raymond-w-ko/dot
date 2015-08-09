72f0e4ad1ac761d709bea1fd535b0134aa7cc2e1
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-08-09 17:55:54 -0400

    add Slamhound, and upgrade lein

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
                   [lein-cljfmt "0.1.10"]
                   [venantius/ultra "0.3.3"]]
-        :dependencies [[cljfmt "0.1.10"]]
+        :dependencies [[cljfmt "0.1.10"]
+                       [slamhound "RELEASE"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :ultra {:color-scheme :solarized_dark}}}
