5d0a5f1480be0deb03f928fee4f9131a46ae14d7
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-09-03 21:23:47 -0400

    add case statements for operating systems

--- bin/start_clipboard_daemon.sh
+++ bin/start_clipboard_daemon.sh
@@ -11,6 +11,36 @@ trap _term SIGINT
 
 clear
 echo "starting network -> clipboard subshell loop"
+case "$(uname -s)" in
+   Darwin)
+     echo "ERROR: do no know how to start clipboard daemon"
+     exit 1
+     ;;
+
+   Linux)
+     echo "ERROR: do no know how to start clipboard daemon"
+     exit 1
+     ;;
+
+   CYGWIN*)
+     (
+     while true; do
+       nc -l 127.0.0.1 64999 | putclip
+     done
+     )
+     ;;
+     
+   MINGW32*|MSYS*)
+     echo "ERROR: do no know how to start clipboard daemon"
+     exit 1
+     ;;
+
+   *)
+     echo "ERROR: do no know how to start clipboard daemon"
+     exit 1
+     ;;
+esac
+
 (
 while true; do
   nc -l 127.0.0.1 64999 | putclip
