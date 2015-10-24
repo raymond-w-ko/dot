45c9842e9033e353f6bb1c232d93595cf3ee2d32
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-10-24 01:04:07 +0000

    add lein-ancient to help upgrade project.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
                   [lein-cljfmt "0.1.10"]
-                  [venantius/ultra "0.3.3"]]
+                  [venantius/ultra "0.3.3"]
+                  [lein-ancient "0.6.7"]]
         :dependencies [[cljfmt "0.1.10"]
                        [slamhound "RELEASE"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
