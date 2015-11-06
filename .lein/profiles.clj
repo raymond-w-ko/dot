e92fca7dd3568e97b0f9f8ddaf12cdc9bfa04d59
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-11-06 11:55:18 -0500

    add Refactor nREPL plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+{:user {:plugins [[refactor-nrepl  "1.1.0"]
+                  [cider/cider-nrepl "0.9.1"]
                   [lein-cljfmt "0.3.0"]
                   [venantius/ultra "0.3.4"]
                   [lein-ancient "0.6.7"]]
