dacf931ec2c3f2952fcf30e00c0bdc710c854280
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-11-06 03:05:14 -0500

    update lein plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,7 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2"]
-                  [lein-cljfmt "0.1.10"]
-                  [venantius/ultra "0.3.3"]
+{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+                  [lein-cljfmt "0.3.0"]
+                  [venantius/ultra "0.3.4"]
                   [lein-ancient "0.6.7"]]
-        :dependencies [[cljfmt "0.1.10"]
-                       [slamhound "RELEASE"]]
+        :dependencies [[slamhound "RELEASE"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :ultra {:color-scheme :solarized_dark}}}
