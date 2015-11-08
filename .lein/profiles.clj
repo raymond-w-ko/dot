c81c64e6970710d598fb28014543369570c0a3ec
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-11-07 22:50:10 -0500

    upgraded global leiningen profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,8 @@
-{:user {:plugins [[refactor-nrepl  "1.1.0"]
+{:user {:plugins [[refactor-nrepl  "1.2.0"]
                   [cider/cider-nrepl "0.9.1"]
                   [lein-cljfmt "0.3.0"]
-                  [venantius/ultra "0.3.4"]
-                  [lein-ancient "0.6.7"]]
-        :dependencies [[slamhound "RELEASE"]]
+                  [venantius/ultra "0.4.0"]
+                  [lein-ancient "0.6.8"]]
+        :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :ultra {:color-scheme :solarized_dark}}}
