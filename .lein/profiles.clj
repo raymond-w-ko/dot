ff4a4d2cd0cf01515d248f4ea42e824f1c0990b7
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-11-17 16:22:55 -0500

    add lein-kibit to check for patterns that can be replaced

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,7 +2,8 @@
                   [cider/cider-nrepl "0.9.1"]
                   [lein-cljfmt "0.3.0"]
                   [venantius/ultra "0.4.0"]
-                  [lein-ancient "0.6.8"]]
+                  [lein-ancient "0.6.8"]
+                  [lein-kibit "0.1.2"]]
         :dependencies [[slamhound "1.5.5"]
                        [cljfmt "0.3.0"]]
         :repl-options {:init (require 'cljfmt.core)}
