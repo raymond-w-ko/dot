a41f4cc9680aba46042474d39f1723104ff674de
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-11-26 12:43:53 -0500

    add code to create .DDS and .PVR textures

--- bin/texturecooker.py
+++ bin/texturecooker.py
@@ -4,7 +4,10 @@ import subprocess
 import codecs
 import re
 
-def squarify(path):
+pvrtextool = "/opt/Imagination/PowerVR_Graphics/PowerVR_Tools/PVRTexTool/CLI/Linux_x86_64/PVRTexToolCLI"
+nvcompress = "/usr/bin/nvcompress"
+
+def cook(path):
   output = subprocess.check_output(["mediainfo", path]).decode("utf-8")
   tokens = re.split(r" |:|\n|\r", output)
   tokens = list(filter(lambda x: len(x) > 0, tokens))
@@ -23,14 +26,25 @@ def squarify(path):
     cmd = ["mogrify", "-resize", "%dx%d!" % (maxdim, maxdim), path]
     print(" ".join(cmd))
     subprocess.check_output(cmd)
+    
+  # make DDS
+  # out = re.sub(r".png$", ".dds", path)
+  # cmd = [nvcompress, "-bc3", "-alpha", path, out]
+  # print(" ".join(cmd))
+  # subprocess.check_output(cmd)
   
+  # make PVR
+  out = re.sub(r".png$", ".pvr", path)
+  cmd = [pvrtextool, "-i", path, "-o", out, "-f", "PVRTC1_4", "-q", "pvrtcbest"]
+  print(" ".join(cmd))
+  subprocess.check_output(cmd)
 
 def main():
   for root, dirs, files in os.walk("."):
     for file in files:
       path = root + os.sep + file
       if file.endswith(".png"):
-        squarify(path)
+        cook(path)
 
 if __name__ == "__main__":
   main()
