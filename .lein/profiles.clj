32164bab3090aa86534ef98ff458bd7dd5b3e8a0
Raymond W. Ko <raymond.w.ko@gmail.com>
2016-05-08 17:36:21 -0400

    upgrade profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,11 @@
 {:user {:plugins [[refactor-nrepl  "2.2.0"]
                   [cider/cider-nrepl "0.12.0"]
-                  [lein-cljfmt "0.5.2"]
+                  [lein-cljfmt "0.5.3"]
                   [venantius/ultra "0.4.1"]
                   [lein-ancient "0.6.10"]
                   [lein-kibit "0.1.2"]]
         :dependencies [[slamhound "1.5.5"]
-                       [cljfmt "0.5.2"]]
+                       [cljfmt "0.5.3"]]
         :repl-options {:init (require 'cljfmt.core)}
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :ultra {:color-scheme :solarized_dark}} }
