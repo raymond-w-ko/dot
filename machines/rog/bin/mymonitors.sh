b7f89c2b5a7d662a5e176e7c6fb4daab549169dc
Raymond W. Ko <raymond.w.ko@gmail.com>
2016-08-09 17:38:20 -0400

    add echo of current monitor configuration messages

--- machines/rog/bin/mymonitors.sh
+++ machines/rog/bin/mymonitors.sh
@@ -4,18 +4,27 @@ isDpOneConnected=$(xrandr | grep 'DP-1 connected' | wc -l)
 isLidOpen=$(cat /proc/acpi/button/lid/LID/state | grep 'open' | wc -l)
 
 if [ "$isDpOneConnected" -eq 1 ] && [ "$isLidOpen" -eq 1 ]; then
+  echo "DisplayPort and Laptop Screen"
   xrandr \
     --output LVDS-0 --auto \
     --output DP-1 --auto --primary --right-of LVDS-0
+elif [ "$isDpOneConnected" -eq 1 ] && [ "$isLidOpen" -eq 0 ]; then
+  echo "DisplayPort Only"
+  xrandr \
+    --output LVDS-0 --off \
+    --output DP-1 --auto --primary
 elif [ "$isHdmiConnected" -eq 1 ] && [ "$isLidOpen" -eq 1 ]; then
+  echo "HDMI and Laptop Screen"
   xrandr \
     --output LVDS-0 --auto \
     --output HDMI-0 --auto --primary --right-of LVDS-0
 elif [ "$isHdmiConnected" -eq 1 ] && [ "$isLidOpen" -eq 0 ]; then
+  echo "HDMI only"
   xrandr \
     --output LVDS-0 --off \
-    --output HDMI-0 --auto --primary --right-of LVDS-0
+    --output HDMI-0 --auto --primary
 else
+  echo "No monitors connected"
   xrandr \
     --output LVDS-0 --auto --primary \
     --output VGA-0 --off \
