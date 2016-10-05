34f9110df147d265cadbec83d89b9b3be2452f33
Raymond W. Ko <raymond.w.ko@gmail.com>
2016-10-05 15:10:39 -0400

    more variations on the script

--- bin/convert_video_for_jeff.sh
+++ bin/convert_video_for_jeff.sh
@@ -9,7 +9,7 @@
 #REM jpeg (screenshot at 10 seconds)
 # -i %1 -ss 00:10 -vframes 1 -r 1 -s 640x360 -f image2 %1.jpg
 
-create_mp4()
+create_dist_mp4()
 {
   SRC=$1  
   DST=converted/${SRC%.*}.mp4
@@ -31,7 +31,29 @@ create_mp4()
     </dev/null
 }
 
+create_master_mp4()
+{
+  SRC=$1  
+  DST=converted/${SRC%.*}.mp4
+
+  echo creating $DST
+
+  rm -f "$DST"
+  nice -n 19 ffmpeg \
+    -i "$SRC" \
+    -threads 8 \
+    -strict -2 \
+    -vcodec libx264 -b:v 1000000k -crf 1 \
+    -profile:v baseline -level:v 3.0 -pix_fmt yuv420p \
+    -movflags +faststart \
+    -acodec aac \
+    -b:a 128k \
+    -ar 44100 \
+    "$DST" \
+    </dev/null
+}
+
 mkdir -p converted
-find . -iname '*.wmv' | while read file; do
-  create_mp4 $file
+find . -iname '*.mp4' | while read file; do
+  create_master_mp4 $file
 done
