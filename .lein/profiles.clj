1f8fe108518bd694d462bf9c3dfa804709c23a00
Raymond W. Ko <raymond.w.ko@gmail.com>
2016-10-28 15:19:19 -0400

    upgrade plugins and deps in lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,11 @@
 {:user {:plugins [[refactor-nrepl  "2.2.0"]
-                  [cider/cider-nrepl "0.13.0"]
-                  [lein-cljfmt "0.5.3"]
-                  [venantius/ultra "0.4.1"]
+                  [cider/cider-nrepl "0.14.0"]
+                  [lein-cljfmt "0.5.6"]
+                  [venantius/ultra "0.5.0"]
                   [lein-ancient "0.6.10"]
                   [lein-kibit "0.1.2"]]
         :dependencies [[slamhound "1.5.5"]
-                       [cljfmt "0.5.3"]]
+                       [cljfmt "0.5.6"]]
         :repl-options {:init (require 'cljfmt.core)}
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :ultra {:color-scheme :solarized_dark}} }
