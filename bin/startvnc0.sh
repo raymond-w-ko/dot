e0bc1657b3293b80c4e60d5003ab7d58c77d50b4
Raymond W. Ko <raymond.w.ko@gmail.com>
2017-02-09 18:54:48 -0500

    remove disabled options

--- bin/startvnc0.sh
+++ bin/startvnc0.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#x0vncserver -display :0 -passwordfile ~/.vnc/passwd
-x11vnc -forever -display :0 -rfbauth $HOME/.vnc/passwd -repeat #-ncache 10 -ncache_cr
+x11vnc -forever -display :0 -rfbauth $HOME/.vnc/passwd -repeat
