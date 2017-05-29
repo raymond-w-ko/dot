19dc876d244d76472a9e348579b461bb9516793d
Raymond W. Ko <raymond.w.ko@gmail.com>
2017-05-28 22:49:06 -0400

    ignore more binaries, always chdir to CWD

--- .vim/bundle/add.sh
+++ .vim/bundle/add.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-find . \( -type f -or -type l \) ! -path '*/.git/*' ! -name '*.pyc' -exec git add -f {} \;
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+find . \( -type f -or -type l \) \
+  ! -path '*/.git/*' \
+  ! -name '*.pyc' \
+  ! -name '*.so' \
+  ! -name '*.o' \
+  ! -name '*.dll' \
+  -exec git add -f {} \;
