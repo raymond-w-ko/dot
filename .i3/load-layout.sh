fa2c2a2815b2b778bc700d8ae3c580d22accb051
Raymond W. Ko <raymond.w.ko@gmail.com>
2018-03-06 09:19:30 -0500

    switch to using thunderbird beta

--- .i3/load-layout.sh
+++ .i3/load-layout.sh
@@ -2,6 +2,6 @@
 i3-msg "workspace 3"
 i3-msg "append_layout ~/.i3/main_workspace.json"
 i3-msg "exec chrome"
-i3-msg "exec thunderbird"
+i3-msg "exec thunderbird-beta"
 i3-msg "exec st"
 i3-msg "exec st"
