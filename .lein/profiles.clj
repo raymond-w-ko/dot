5e526e5eeab911c94e019c2c59f877b713c52214
Raymond W. Ko <raymond.w.ko@gmail.com>
2018-10-05 09:01:21 -0400

    use most recent cider/cider-nrepl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,8 @@
 {:user {:plugins [[lein-cljfmt "0.6.1"]
                   [venantius/ultra "0.5.2"]
                   [lein-ancient "0.6.15"]
-                  [lein-kibit "0.1.6"]]
+                  [lein-kibit "0.1.6"]
+                  [cider/cider-nrepl "0.18.0"]]
         :dependencies [[slamhound "1.5.5"]
                        [cljfmt "0.6.1"]]
         :repl-options {:init (require 'cljfmt.core)}
