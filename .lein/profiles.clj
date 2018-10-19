7cde73d4d7a27791e05786d7c20caa8ea51f2d4e
Raymond W. Ko <raymond.w.ko@gmail.com>
2018-10-19 00:24:50 -0400

    lein/profiles.clj: remove cider-nrepl to be safe

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,7 @@
 {:user {:plugins [[lein-cljfmt "0.6.1"]
                   [venantius/ultra "0.5.2"]
                   [lein-ancient "0.6.15"]
-                  [lein-kibit "0.1.6"]
-                  [cider/cider-nrepl "0.18.0"]]
+                  [lein-kibit "0.1.6"]]
         :dependencies [[slamhound "1.5.5"]
                        [cljfmt "0.6.1"]]
         :repl-options {:init (require 'cljfmt.core)}
