697dcea663f713da8b2572bf37f1976ff930f3e3
Raymond W. Ko <raymond.w.ko@gmail.com>
2019-02-08 16:28:12 -0500

    lein: update dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
-{:user {:plugins [[lein-cljfmt "0.6.3"]
+{:user {:plugins [[lein-cljfmt "0.6.4"]
                   [lein-ancient "0.6.15"]
                   [lein-kibit "0.1.6"]]
         :dependencies [[org.clojure/clojure "1.10.0"]
                        [slamhound "1.5.5"]
-                       [cljfmt "0.6.3"]]
+                       [cljfmt "0.6.4"]]
         :repl-options {:init (require 'cljfmt.core)}
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         }}
