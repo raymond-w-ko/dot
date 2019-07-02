6c684900d6605e5823b38b5414374423fdd82b98
Raymond W. Ko <raymond.w.ko@gmail.com>
2019-07-02 15:56:04 -0400

    update to clojure 1.10.1

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:plugins [[lein-cljfmt "0.6.4"]
                   [lein-ancient "0.6.15"]
                   [lein-kibit "0.1.6"]]
-        :dependencies [[org.clojure/clojure "1.10.0"]
+        :dependencies [[org.clojure/clojure "1.10.1"]
                        [slamhound "1.5.5"]
                        [cljfmt "0.6.4"]]
         :repl-options {:init (require 'cljfmt.core)}
