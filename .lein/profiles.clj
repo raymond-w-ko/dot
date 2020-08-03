a2eca6199524aac42518db1bbd37cf97ebde9711
Raymond W. Ko <raymond.w.ko@gmail.com>
2020-08-03 12:02:15 -0400

    lein: update cljfmt

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
-{:user {:plugins [[lein-cljfmt "0.6.7"]
+{:user {:plugins [[lein-cljfmt "0.6.8"]
                   [lein-kibit "0.1.8"]
                   [jonase/eastwood "0.3.11"]]
         :dependencies [[org.clojure/clojure "1.10.1"]
                        [slamhound "1.5.5"]
-                       [cljfmt "0.6.7"]]
+                       [cljfmt "0.6.8"]]
         :repl-options {:init (require 'cljfmt.core)}
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         }}
