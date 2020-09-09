e386195e57f15f42560d12d7a004672a82f4e8d6
Raymond W. Ko <raymond.w.ko@gmail.com>
2020-09-08 21:37:09 -0400

    lein/profiles.clj: update version, remove obsolete, unused

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,8 @@
-{:user {:plugins [[lein-cljfmt "0.6.8"]
+{:user {:plugins [[lein-cljfmt "0.7.0"]
                   [lein-kibit "0.1.8"]
                   [jonase/eastwood "0.3.11"]]
         :dependencies [[org.clojure/clojure "1.10.1"]
-                       [slamhound "1.5.5"]
-                       [cljfmt "0.6.8"]]
+                       [cljfmt "0.7.0"]]
         :repl-options {:init (require 'cljfmt.core)}
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :aliases {}
         }}
