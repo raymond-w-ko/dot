a53be792e43a298e0fad29adf46bc5f75d3d1943
Raymond W. Ko <raymond.w.ko@gmail.com>
2021-03-01 15:26:48 -0500

    fix wrong inclusions

--- commit-vim-bundles.sh
+++ commit-vim-bundles.sh
@@ -4,10 +4,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR/.vim/bundle/"
 find . \( -type f -or -type l \) \
   ! -path '*/.git/*' \
+  ! -path '*/.cpcache/*' \
+  ! -path '*/server/geckocomplete/target/*' \
   ! -name '*.pyc' \
+  ! -name '*.sock' \
   ! -name '*.so' \
   ! -name '*.o' \
   ! -name '*.dll' \
   ! -name '*.exe' \
+  ! -name '*.class' \
   -exec git add -f --verbose {} \;
 git commit -m "updated vim bundle(s)" .
