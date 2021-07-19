c70623fe160d6b5bc3f873270af7ced24f5c4ee9
Raymond W. Ko <raymond.w.ko@gmail.com>
2021-07-19 00:45:50 -0400

    enable incremental selection

--- .vim/lua/rko.lua
+++ .vim/lua/rko.lua
@@ -6,6 +6,15 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
     disable = { "edn", "json" },
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
   indent = {
     enable = true,
   },
