42f5ed49bd79961353181844723c9b9899075f50
Raymond W. Ko <raymond.w.ko@gmail.com>
2022-03-09 00:20:48 -0500

    my-terminal.sh: use kitty first

--- bin/my-terminal.sh
+++ bin/my-terminal.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if hash st 2>/dev/null ; then
+if hash kitty 2>/dev/null ; then
+  exec kitty
+elif hash st 2>/dev/null ; then
   exec st
 elif hash urxvt 2>/dev/null; then
   exec urxvt
