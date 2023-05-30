81d247a2a5b008e4ddf7e46edc40474a37173ab2
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-05-29 23:40:16 -0400

    emacs: try fixing eln-cache in .emacs.d

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,1 +1,11 @@
+(set-default-coding-systems 'utf-8)
+(setq gc-cons-threshold (* 100 1000 1000))
+(setq read-process-output-max (* 1024 1024))
 (setq package-enable-at-startup nil)
+
+;; make emacs not store in a version controlled directory
+(setq user-emacs-directory (expand-file-name "~/.cache/emacs/")
+      url-history-file (expand-file-name "url/history" user-emacs-directory))
+(make-directory user-emacs-directory t)
+(when (boundp 'native-comp-eln-load-path)
+  (startup-redirect-eln-cache (expand-file-name "eln-cache" user-emacs-directory)))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,15 +1,3 @@
-;; emacs
-(setq gc-cons-threshold (* 100 1000 1000))
-(set-default-coding-systems 'utf-8)
-(setq read-process-output-max (* 1024 1024))
-
-;; make emacs not store in a version controlled directory
-(setq user-emacs-directory (expand-file-name "~/.cache/emacs/")
-      url-history-file (expand-file-name "url/history" user-emacs-directory))
-(make-directory user-emacs-directory t)
-(when (boundp 'native-comp-eln-load-path)
-  (startup-redirect-eln-cache (expand-file-name "eln-cache" user-emacs-directory)))
-
 ;; package manager
 (defvar bootstrap-version)
 (let ((bootstrap-file
