5bc7458cdb227f4f6a03776b71ce865732f68901
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-07-14 13:32:56 -0400

    emacs: new tab fn and window splitting

--- .emacs.d/rko-lisp/rko-interactives.el
+++ .emacs.d/rko-lisp/rko-interactives.el
@@ -2,6 +2,12 @@
 
 (require 'dash)
 
+(defun rko/set-current-window-text-width (w)
+  (let* ((win (selected-window))
+         (delta (- w (window-width win))))
+    (message "delta %s" delta)
+    (window-resize win delta t)))
+
 (defun rko/tab-new ()
   (interactive)
 
@@ -11,19 +17,20 @@
     (when (or (> num-tabs 1)
               (> num-wins 1))
       (tab-new)))
-  
-  (split-window-right)
-  (split-window-below)
-  (windmove-right)
-  
-  ;; (split-window-right)
-  ;; (split-window-below)
-  ;; (windmove-right)
-  
-  (split-window-below)
-  (windmove-left)
-  
-  (balance-windows))
+
+  (let* ((fw (frame-width))
+         (n (floor (/ fw 100)))
+         (rem (- fw (* n 100)
+                 12)))
+    (dotimes (_ n)
+      (split-window-right))
+    (rko/set-current-window-text-width rem)
+    (dotimes (_ n)
+      (windmove-right)
+      (rko/set-current-window-text-width 100))
+    (dotimes (_ n)
+      (split-window-below)
+      (windmove-left))))
 (keymap-global-set "C-x t 2" 'rko/tab-new)
 
 (provide 'rko-interactives)
