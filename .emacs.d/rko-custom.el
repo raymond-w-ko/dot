1e917b34c8ec61f07c7fbe8dad95f9d0d0a3d92f
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-10-06 15:44:53 -0400

    emacs: save tweaks

--- .emacs.d/rko-custom.el
+++ .emacs.d/rko-custom.el
@@ -128,12 +128,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:slant normal :weight normal :width normal :height 80 :family "Iosevka Comfy Wide"))))
+ '(default ((t (:slant normal :weight normal :width normal :height 100 :family "JetBrains Mono"))))
  '(fixed-pitch ((t :inherit default)))
- '(fixed-pitch-serif ((t :inherit default :family "Iosevka Comfy Motion")))
+ '(fixed-pitch-serif ((t :inherit default :family "JetBrains Mono")))
  '(hop-face-double-char-1 ((t (:inherit ef-themes-mark-delete))))
  '(hop-face-double-char-2 ((t (:inherit ef-themes-mark-other))))
  '(hop-face-single-char ((t (:inherit ef-themes-mark-select))))
  '(italic ((t (:slant normal))))
  '(mono-complete-preview-face ((t :inherit font-lock-comment-face)))
- '(variable-pitch ((t (:slant normal :weight normal :width normal :height 90 :family "Iosevka Comfy Duo")))))
+ '(variable-pitch ((t (:slant normal :weight normal :width normal :height 90 :family "JetBrains Mono")))))
--- .emacs.d/rko-lisp/rko-interactives.el
+++ .emacs.d/rko-lisp/rko-interactives.el
@@ -7,31 +7,6 @@
          (delta (- w (window-width win))))
     (window-resize win delta t)))
 
-(defun rko/tab-new-old ()
-  (interactive)
-
-  (let* ((num-tabs (->> (frame-parameter (selected-frame) 'tabs)
-                        (length)))
-         (num-wins (length (window-list))))
-    (when (or (> num-tabs 1)
-              (> num-wins 1))
-      (tab-new)))
-
-  (let* ((fw (frame-width))
-         (target-width 102)
-         (n (floor (/ fw target-width)))
-         (rem (- fw (* n 100)
-                 10)))
-    (dotimes (_ n)
-      (split-window-right))
-    (rko/set-current-window-text-width rem)
-    (dotimes (_ n)
-      (windmove-right)
-      (rko/set-current-window-text-width target-width))
-    (dotimes (_ n)
-      (split-window-below)
-      (windmove-left))))
-
 (defun rko/tab-new (universal-arg)
   (interactive "p")
 
@@ -42,7 +17,7 @@
               (> num-wins 1))
       (tab-new)))
 
-  (let* ((n 2)
+  (let* ((n 3)
          (split-more (if (< 0 universal-arg)
                          (/ universal-arg 4)
                        0)))
