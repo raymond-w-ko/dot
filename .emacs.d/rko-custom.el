08d8be75d28f0e1ff1224389a6c3e165214a2594
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-11-24 02:24:08 -0500

    emacs: save tweaks

--- .emacs.d/rko-custom.el
+++ .emacs.d/rko-custom.el
@@ -128,7 +128,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:slant normal :weight normal :width normal :height 100 :family "Fira Code"))))
+ '(default ((t (:slant normal :weight medium :width normal :height 100 :family "Fira Code"))))
  '(fixed-pitch ((t :inherit default)))
  '(fixed-pitch-serif ((t :inherit default :family "Fira Code")))
  '(hop-face-double-char-1 ((t (:inherit ef-themes-mark-delete))))
