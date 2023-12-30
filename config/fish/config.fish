2e6155f5969e58519dce418f2247365f39085102
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-12-30 04:09:58 -0500

    fish: save abbr

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+abbr -a -- gp 'git push'
+abbr -a -- gs 'git status'
+abbr -a -- gd 'git diff'
+abbr -a -- gc 'git commit'
+abbr -a -- ga 'git add'
