abf5aba2118a261d46644bdb9df438d4a024e459
Raymond W. Ko <raymond.w.ko@gmail.com>
2024-12-26 15:19:56 -0500

    fix killall-vscode-server warnings

--- bin/killall-vscode-server.sh
+++ bin/killall-vscode-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ps uxa | grep .vscode-server | grep -v grep | grep -v killall-vscode-server.sh | awk '{print $2}' | xargs kill
-ps uxa | grep .cursor-server | grep -v grep | grep -v killall-vscode-server.sh | awk '{print $2}' | xargs kill
+ps uxa | grep .vscode-server | grep -v grep | grep -v killall-vscode-server.sh | awk '{print $2}' | xargs -r kill
+ps uxa | grep .cursor-server | grep -v grep | grep -v killall-vscode-server.sh | awk '{print $2}' | xargs -r kill
