fdf81c06ccc1b0ac4d4b7727e334198297d6b3ae
Raymond W. Ko <raymond.w.ko@gmail.com>
2025-01-11 21:45:28 +0000

    fish: re-enable conda

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -16,9 +16,9 @@ source $HOME/dot/fisher/functions/fisher.fish
 
 #>>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-# if test -f ~/miniconda3/bin/conda
-#   eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-# end
+if test -f ~/miniconda3/bin/conda
+  eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+end
 # <<< conda initialize <<<
 
 if status is-interactive
